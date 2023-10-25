13be62d7f6f5a53e4615f5f15f8a7c1bd82915ae
Gabriel arduvini <gabrielarduvini@outlook.com>
2023-10-25 14:47:54 -0300

    cadastro-prestador CSS

--- frontend/src/components/RegisterForm.js
+++ frontend/src/components/RegisterForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import '../pages/Menu.css'
+import '../pages/Menu.css';
+import './cadastro-instituicao.css';
 
 function RegisterForm({ onRegister }) {
   const [formData, setFormData] = useState({
@@ -28,144 +29,186 @@ function RegisterForm({ onRegister }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Tipo:
-        <select name="user_type" value={formData.user_type} onChange={handleChange}>
+   <div>
+
+    <div class="cadastro-prestador">
+
+      <form onSubmit={handleSubmit}>
+       <div class="menu-cadastro">
+          <h1 class="titulo-cadastro">Cadastro</h1>
+         {/*  talvez não ira ser usador
+
+          <label class="">
+          Tipo:
+          <select name="user_type" value={formData.user_type} onChange={handleChange}>
           <option value="PERSON">Pessoa</option>
           <option value="ONG">ONG</option>
-        </select>
-      </label>
-      <br />
+          </select><p></p>
+          </label>
+          <br/>
+        */} 
 
-      <label>
-        CPF/CNPJ:
-        <input
-          type="text"
-          name="cpf_cnpj"
-          value={formData.cpf_cnpj}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <br />
+          <label >
+          <input
+            class="texto-cadastro"
+            type="text"
+            placeholder=' Nome Completo'
+            name="full_name"
+            value={formData.full_name}
+            onChange={handleChange}
+            required
+            />
+          </label>
+          <br />
 
-      <label>
-        Nome Completo/Organização:
-        <input
-          type="text"
-          name="full_name"
-          value={formData.full_name}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <br />
+          <label>
+          <input
+            class="texto-cadastro"
+            type="text"
+            placeholder=' CPF'
+            name="cpf_cnpj"
+            value={formData.cpf_cnpj}
+            onChange={handleChange}
+            required
+            />
+          </label>
+          <br/>
 
-      <label>
+          <label >
+          <input
+            class="texto-cadastro"
+            type="text"
+            placeholder=' Usuario'
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+            required
+            />
+          </label>
+          <br />
+
+          <label >
+          <input
+
+type="email"
+            class="texto-cadastro"
+            placeholder=' E-mail'
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            required
+          />
+          </label>
+          <br />
+          <label >
+          <input
+            class="texto-cadastro"
+            type="tel"
+            placeholder=' Telefone'
+            name="phone"
+            value={formData.phone}
+            onChange={handleChange}
+            />
+          </label>
+          <br />
+
+          <label >
+          <input
+            class="texto-cadastro"
+            type="password"
+            placeholder=' Senha'
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            required
+            />
+          </label>
+          <br />
+
+
+
+
+           {/*  dopo a não ser utilizado
+         <label class="">
+          <input
+            type="text"
+            placeholder=' Endereço'
+            name="address"
+            value={formData.address}
+            onChange={handleChange}
+            />
+            </label >
+            <br />
+            
+            <label class="">
+            <input
+            type="text"
+            placeholder=' Cidade'
+            name="city"
+            value={formData.city}
+            onChange={handleChange}
+          />
+          </label>
+          <br />
+
+          <label class="">
+          <input
+            type="text"
+            placeholder=' Estado'
+            name="state"
+            value={formData.state}
+            onChange={handleChange}
+            />
+            </label>
+            <br />
+            
+            <label class="">          
+            <input
+            type="text"
+            placeholder=' CEP'
+            name="zip_code"
+            value={formData.zip_code}
+            onChange={handleChange}
+            />
+            </label>
+            <br />
+            
+            <label class="">
+            Descrição:
+            <textarea
+            name="description"
+            value={formData.description}
+             onChange={handleChange}
+             />
+             </label>
+             <br />
+            */}   
+
+          <button class="botao-cadastro" type="submit">Registrar</button>
+        </div> 
         
-        Username:
-        <input
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <br />
+      </form>
+      
+    </div>
+    <div class="rodape-footer">
+    <div class="footer-esquerda">
 
-      <label>
-        Email:
-        <input
-          type="email"
-          name="email"
-          value={formData.email}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <br />
+    </div>
+    <div class="footer-direita">
+        
+            <h3 class="footer-texto">Home</h3>
+        
+            <h3 class="footer-texto">Sobre</h3>
+        
+            <h3 class="footer-texto">Contato</h3>
+        
+            <h3 class="footer-texto">login</h3>
 
-      <label>
-        Senha:
-        <input
-          type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <br />
+            <h3 class="footer-texto">Registrar</h3>
 
-      <label>
-        Telefone:
-        <input
-          type="tel"
-          name="phone"
-          value={formData.phone}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      <label>
-        Endereço:
-        <input
-          type="text"
-          name="address"
-          value={formData.address}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      <label>
-        Cidade:
-        <input
-          type="text"
-          name="city"
-          value={formData.city}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      <label>
-        Estado:
-        <input
-          type="text"
-          name="state"
-          value={formData.state}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      <label>
-        CEP:
-        <input
-          type="text"
-          name="zip_code"
-          value={formData.zip_code}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      <label>
-        Descrição:
-        <textarea
-          name="description"
-          value={formData.description}
-          onChange={handleChange}
-        />
-      </label>
-      <br />
-
-      <button type="submit">Registrar</button>
-    </form>
+    </div>
+</div>
+            </div> 
   );
 }
 
